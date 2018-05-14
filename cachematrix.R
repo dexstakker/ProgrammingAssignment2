aae77bd493927dee9f5f80cb8060b92ce2c6a609
Jason Graham <dex@Jasons-MBP.attlocal.net>
2018-05-14 15:46:54 -0700

    Changed function names back to what was requested

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,16 @@
-## This file defines a function, makeCachingMatrix(), that creates a matix that is 
+## This file defines a function, makeCacheMatrix(), that creates a matix that is 
 ## capable of caching the results of a calculation of its inverse 
 ## so that it can later be retrieved without recalculting 
 
-## In addition, it defines another function, cachingSolve(), that 
-## calculates the inverse of a matrirx created by makeCachingMatrix
+## In addition, it defines another function, cacheSolve(), that 
+## calculates the inverse of a matrirx created by makeCacheMatrix
 ## if it had not already been calculated for the current value of 
 ## its matrix. If it had, it returns the previously calculated 
 ## inverse
 
 ## Creates a matrix that is automatically capable of 
 ## caching its calculated inverse
-makeCachingMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
   if (nrow(x) != ncol(x) || nrow(x) <= 0) {
     stop("invalid matrix size: shoud be square matrix of non-zero size")
   }
@@ -21,7 +21,7 @@ makeCachingMatrix <- function(x = matrix()) {
   m <- NULL
   
   # set() sets the overall matrix value, it assigns parameter matrix "y"
-  # to the "x" one level up (inside of makeCachingMatrix), and
+  # to the "x" one level up (inside of makeCacheMatrix), and
   # clears the "m" one-level up of it's cached value because 
   # we changed the base matrix
   set <- function(y) {
@@ -29,17 +29,17 @@ makeCachingMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   
-  # get() - a function that returns possibly modified makeCachingMatrix
+  # get() - a function that returns possibly modified makeCacheMatrix
   # parameter matrix "x"
   get <- function() x
 
   # setsolve(solve) a function that takes a matrix parameter
   # for the invertedMatrix and saves it in the "m" defined 1 level)
-  # up from this function: inside makeCachingMatrix
+  # up from this function: inside makeCacheMatrix
   setsolve <- function(solvedMatrix) m <<- solvedMatrix
   
   # getsolve  function returns the inverted matrix stored in 
-  # "m" one level up in makeCachingMatrix function
+  # "m" one level up in makeCacheMatrix function
   getsolve <- function() m 
 
   # list of four basic function defined above, names
@@ -50,11 +50,11 @@ makeCachingMatrix <- function(x = matrix()) {
 }
 
 
-## cachingSolve(x) is a function that takes new cachingMatrix as input and 
+## cacheSolve(x) is a function that takes new caching matrix as input and 
 ## can return the cachced value if available of calculate the
 ## new one if not
 
-cachingSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {
   print(x)
   ## Return the matrix that is cached currently as inverse of 'x'
   invMat <- x$getsolve()
